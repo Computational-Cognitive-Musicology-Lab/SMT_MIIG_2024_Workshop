ca0ffb28b15f87dee9788d0f95a79abd64305a7d
ncondits3 <ncondits3@gatech.edu>
2024-11-08 07:19:19 -0500

    Modified the CoCoPops files, so they are simpler.

--- Tutorials/helper_functions.R
+++ Tutorials/helper_functions.R
@@ -1,15 +1,54 @@
-pline <- function() {
-  plot.new() ; plot.window(c(0,1), c(0, 1))
+textsize <- .5
+
+pitch_space1 <- function(x = 'Acoustic', y = 'Tonal') {
+  plot.new() ; plot.window(c(-.2,1.2), c(-.2, 1.2))
 
 arrows(0, .5, 1, .5, code = 3, length = .1, lwd = 2)
 
-text(0, .5, pos = 2, "Acoustic", xpd = TRUE)
-text(1, .5, pos = 4, "Tonal", xpd = TRUE)
-title('"Pitch"')
+par(family = 'Roboto', mar = c(0,0,0,0))
+text(0, .5, pos = 2, x ,xpd = TRUE, font = 2, cex = .8)
+text(1, .5, pos = 4, y, xpd = TRUE, font = 2, cex = .8)
+# title('"Pitch"')
 }
 
 lab <- function(n, text) {
-     text <- gsub(' ', '\n', text)
-
-     text(n, .45, pos = 1, text)
+     text <- paste(text, collapse = '\n')
+     par(family = 'Roboto')
+     text(n, .4, pos = 1, text, cex = textsize, srt = 90)
 }
+
+pitch_space2 <- function(x, y, i, j) {
+  plot.new()
+  plot.window(c(-.2, 1.2), c(-.2, 1.2))
+  par(family = 'Roboto', mar = c(0,0,0,0))
+  
+  arrows(0, .5, 1, .5, code = 3, length = .1, lwd = 2)
+  arrows(.5, 0, .5, 1, code = 3, length = .1, lwd = 2)
+  text(0, .5, pos = 2, x, xpd = TRUE, font = 2, cex = .8)
+  text(1, .5, pos = 4, y, xpd = TRUE, font = 2, cex = .8)
+  text(.5, 0, pos = 1, i, xpd = TRUE, font = 2, cex = .8)
+  text(.5, 1, pos = 3, j, xpd = TRUE, font = 2, cex = .8)
+  
+}
+
+lab2 <- function(x, y, text, family = 'Roboto') {
+  
+  par(family = family)
+  text(x, y, text, xpd = TRUE, cex = textsize)
+}
+
+
+# Prepreocessing of cocopops
+
+if (FALSE) {
+cocopops <- readHumdrum('Data/CoCoPops/.*hum', reference = c('BillboardChartDate', 'BillboardPeak', 'COC', 'OTL'))
+  
+
+cocopops[[,c('**kern','**harm', '**silbe', '**rms')]][[, kern = 1, harm = 1, silbe = 1]] -> cocopops
+
+cocopops |> group_by(File) |> filter(any(Exclusive == 'kern')) |> ungroup() -> cocopops
+
+# writeHumdrum(cocopops)
+save(cocopops, file = 'Data/CoCoPops/CoCoPops.Rd', compress = TRUE)
+}
+
