12a8d4fb5ec0518175603f3d88d3cf4fb91ddcad
ncondits3 <ncondits3@gatech.edu>
2024-11-08 21:30:46 -0500

    Made progress on my tutorial.

--- Tutorials/helper_functions.R
+++ Tutorials/helper_functions.R
@@ -3,18 +3,18 @@ textsize <- .5
 pitch_space1 <- function(x = 'Acoustic', y = 'Tonal') {
   plot.new() ; plot.window(c(-.2,1.2), c(-.2, 1.2))
 
-arrows(0, .5, 1, .5, code = 3, length = .1, lwd = 2)
+arrows(0.5, 0, .5, 1, code = 3, length = .1, lwd = 2)
 
 par(family = 'Roboto', mar = c(0,0,0,0))
-text(0, .5, pos = 2, x ,xpd = TRUE, font = 2, cex = .8)
-text(1, .5, pos = 4, y, xpd = TRUE, font = 2, cex = .8)
+text(.5, 0, pos = 1, x ,xpd = TRUE, font = 2, cex = .8)
+text(.5, 1, pos = 3, y, xpd = TRUE, font = 2, cex = .8)
 # title('"Pitch"')
 }
 
 lab <- function(n, text) {
      text <- paste(text, collapse = '\n')
      par(family = 'Roboto')
-     text(n, .4, pos = 1, text, cex = textsize, srt = 90)
+     text(.5, n, pos = 4, text, cex = textsize)
 }
 
 pitch_space2 <- function(x, y, i, j) {
